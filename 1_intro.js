a4a69d2a1bb5adf728fca1aefc0da303836cab67
ashish-509 <101721265+ashish-509@users.noreply.github.com>
2024-01-06 22:14:07 +0545

    Add files via upload

---
+++ 1_intro.js
@@ -0,0 +1,5 @@
+var user = { name: "Ashish", age: 18 };
+console.log("My name is Ashish.");
+console.log(user.name);
+// tsc filename.ts
+// run this typescript file by writing .... tsc filename.ts ('tsc 1_intro.ts' in this case) and it automatically produces the another .js file with same filename and the equivalent code in javaScript.
