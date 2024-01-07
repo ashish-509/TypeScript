e7e08fe44f98185d3e200ae287743747fdb5a89c
ashish-509 <101721265+ashish-509@users.noreply.github.com>
2024-01-07 08:58:26 +0545

    Add files via upload

---
+++ 2_variables.js
@@ -0,0 +1,4 @@
+// Syntax : 
+//  let variableName : type = value
+var greetings = "Hello Ashish !";
+console.log(greetings);
---
+++ 2_variables.ts
@@ -0,0 +1,10 @@
+// Syntax : 
+//  let variableName : type = value
+
+let greetings : string = "Hello Ashish !";
+console.log(greetings);
+
+// ran the program with ( tsc 2_variables.ts ) in the terminal which created equivalent .js file as well.
+
+export {} // temporarily removes the error.
+
