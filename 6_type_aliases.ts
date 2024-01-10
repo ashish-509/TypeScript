3f271ec0dee6c7206abfa2d5b97457e147d75c4a
ashish-509 <101721265+ashish-509@users.noreply.github.com>
2024-01-10 07:39:11 +0545

    Add files via upload

---
+++ 6_type_aliases.ts
@@ -0,0 +1,14 @@
+type User = {
+    name : string;
+    level : string;
+    percentage : number;
+    hasPassed : boolean;
+}
+
+function createUser (user : User){
+    return user;
+}
+
+
+
+export{}
